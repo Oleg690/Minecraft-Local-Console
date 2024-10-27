e759d64e003e477aae13747405d537607cdb8500
Oleg690 <100958276+Oleg690@users.noreply.github.com>
2024-10-27 09:18:15 +0200

    for good

--- v1.1 - Copy/databaseCreate.py
+++ v1.1 - Copy/databaseCreate.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect('D:\\Mineraft Server\\v1.1 - Copy\\database\\worldsData.db')
+connection = sqlite3.connect('database\\worldsData.db')
 cursor = connection.cursor()
 
 def createTable(sql):
