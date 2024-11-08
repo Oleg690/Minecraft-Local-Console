dbb0fbcd977647adb56a2f64a32e242659c9a99a
Oleg690 <100958276+Oleg690@users.noreply.github.com>
2024-11-08 16:19:02 +0200

    .

--- v1.1 - Copy/databaseCreate.py
+++ v1.1 - Copy/databaseCreate.py
@@ -3,11 +3,15 @@ import sqlite3
 connection = sqlite3.connect('database\\worldsData.db')
 cursor = connection.cursor()
 
+tableName = 'worlds'
+insertOneDefaultSQLVerificator = True
+insertOneDefaultSQL = f'''insert into {tableName} (worldNumber, name, version, totalPlayers, rconPassword) values('123456789', 'Minecraft SMP', '1.21', '20', '123456789123456789');'''
+
 def createTable(sql):
     cursor.execute(sql)
     connection.commit()
 
-createTableSQL = '''CREATE TABLE IF NOT EXISTS worlds(
+createTableSQL = f'''CREATE TABLE IF NOT EXISTS {tableName}(
     id integer primary key autoincrement,
     worldNumber text,
     name text,
@@ -16,16 +20,11 @@ createTableSQL = '''CREATE TABLE IF NOT EXISTS worlds(
     rconPassword text
     )'''
 
-insertOneDefaultSQLVerificator = False
-insertOneDefaultSQL = '''insert into worlds (worldNumber, name, version, totalPlayers, rconPassword) values('123456789', 'Minecraft SMP', '1.21', '20', '123456789123456789');'''
-
 createTable(createTableSQL)
 print('Database created succeasfully!')
 
 if insertOneDefaultSQLVerificator == True:
     createTable(insertOneDefaultSQL)
     print('Default data inserted succeasfully!')
-
-
 
 connection.close()
