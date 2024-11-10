03fdd50741ac2effb3f4227ab4e1971dfa0a91e0
Oleg690 <100958276+Oleg690@users.noreply.github.com>
2024-11-10 18:50:05 +0200

    .

--- v1.1/web-server.py
+++ v1.1/web-server.py
@@ -1,6 +1,8 @@
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
-server_address = ('', 2000)
+port = 2000
+
+server_address = ('', port)
 httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
 print('Start')
 httpd.serve_forever()
